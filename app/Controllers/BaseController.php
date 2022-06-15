12b163d11e35c97ae93ebe67bf2985290038a590
vikrant sharma <infovikrantsharma@gmail.com>
2022-06-15 17:53:55 +0530

    File Updated

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -36,7 +36,7 @@ class BaseController extends Controller
      *
      * @var array
      */
-    protected $helpers = ["Basic","Form"];
+    protected $helpers = ["form", "Basic"];
 
     /**
      * Constructor.
